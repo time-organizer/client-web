c39da50c83f1827377c9c93862f1a27f63f562db
Michał Grabowski <Miriloth@gmail.com>
2018-09-11 19:48:12 +0200

    Add pre-commit

--- src/__test__/scenes/App.test.js
+++ src/__test__/scenes/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/scenes/App';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../../scenes/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render((
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+    , div);
   ReactDOM.unmountComponentAtNode(div);
 });
