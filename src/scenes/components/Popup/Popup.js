66c325ec3f793bfc9036e4ee3599d9306a361a94
Michał Grabowski <Miriloth@gmail.com>
2018-10-07 22:46:58 +0200

    Animate popup appearing

--- src/scenes/components/Popup/Popup.js
+++ src/scenes/components/Popup/Popup.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
 
 import Portal from '../Portal';
 import CloseButton from '../CloseButton';
@@ -13,20 +14,58 @@ export const popupTypes = {
   fullscreen: 'fullscreen',
 };
 
-const Popup = ({
-  onClose, children, popupType, withCloseButton,
-}) => (
-  <Portal>
-    <div className="popup-overlay" onClick={onClose}>
-      <div className={`popup ${popupType}`}>
-        {withCloseButton && (
-          <CloseButton onClose={onClose} />
-        )}
-        {children}
-      </div>
-    </div>
-  </Portal>
-);
+class Popup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animationTriggered: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ animationTriggered: true });
+  }
+
+  closePopup = () => {
+    const { onClose } = this.props;
+    this.setState({ animationTriggered: false });
+
+    setTimeout(onClose, 300);
+  };
+
+  render() {
+    const {
+      children, popupType, withCloseButton,
+    } = this.props;
+    const { animationTriggered } = this.state;
+
+    return (
+      <Portal>
+        <CSSTransition
+          in={animationTriggered}
+          classNames="popup-overlay-transition"
+          unmountOnExit
+          timeout={300}
+        >
+          <div className="popup-overlay" onClick={this.closePopup} />
+        </CSSTransition>
+        <CSSTransition
+          in={animationTriggered}
+          classNames="popup-transition"
+          unmountOnExit
+          timeout={300}
+        >
+          <div className={`popup ${popupType}`}>
+            {withCloseButton && (
+              <CloseButton onClose={this.closePopup} />
+            )}
+            {children}
+          </div>
+        </CSSTransition>
+      </Portal>
+    );
+  }
+}
 
 Popup.propTypes = {
   onClose: PropTypes.func.isRequired,
