2d326f855abc6ad661f59e37b9971d41fd01dd8e
Michał Grabowski <Miriloth@gmail.com>
2018-10-21 17:02:07 +0200

    Animate list items appearing

--- src/scenes/App/scenes/Boards/components/BoardsList/BoardsList.js
+++ src/scenes/App/scenes/Boards/components/BoardsList/BoardsList.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import map from 'lodash/map';
 import PropTypes from 'prop-types';
+import {
+  TransitionGroup,
+} from 'react-transition-group';
 
 import NewBoardButton from '../NewBoardButton';
 import BoardListItem from '../BoardListItem';
 import BoardModel from '../../../../../../models/Board';
+import Fade from '../../../../../components/transitions/Fade';
 
 import './BoardsList.css';
 
 const BoardsList = ({ boards }) => (
-  <div className="boards-list">
+  <TransitionGroup className="boards-list">
     <NewBoardButton />
     {map(boards, board => (
-      <BoardListItem
-        key={board._id}
-        board={board}
-      />
+      <Fade trigger>
+        <BoardListItem
+          key={board._id}
+          board={board}
+        />
+      </Fade>
     ))}
-  </div>
+  </TransitionGroup>
 );
 
 BoardsList.propTypes = {
