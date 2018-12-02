308ec0667a0080f72638d46d2d472706a3e1e35b
Michał Grabowski <Miriloth@gmail.com>
2018-12-02 18:51:56 +0100

    Remove sizes from asset model

--- src/models/Asset.js
+++ src/models/Asset.js
@@ -4,10 +4,6 @@ const AssetModel = {
   propTypes: PropTypes.oneOfType([
     PropTypes.shape({
       assetId: PropTypes.string.isRequired,
-      size: PropTypes.shape({
-        width: PropTypes.number.isRequired,
-        height: PropTypes.number.isRequired,
-      }).isRequired,
     }),
   ]),
 };
